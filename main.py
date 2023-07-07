e684782f4319ca076a59f0eeebd7e65c18c180d1
Lukas Olson <lukasdragon@live.ca>
2023-07-07 11:17:54 -0700

    Added the first graph

--- main.py
+++ main.py
@@ -1,63 +1,31 @@
 import json
-
 import requests
 import streamlit as st
 import numpy as np
+import pandas as pd
+from datetime import date, timedelta, datetime
 
-from datetime import datetime
-
-api_url = "https://hacvffgmaquyyeiusnbi.supabase.co/rest/v1/"
-api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYm" \
+API_URL = "https://hacvffgmaquyyeiusnbi.supabase.co/rest/v1/"
+API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYm" \
           "FzZSIsInJlZiI6ImhhY3ZmZmdtYXF1eXllaXVzbmJpIiwicm9sZSI6Im" \
           "Fub24iLCJpYXQiOjE2ODczMDI5NDQsImV4cCI6MjAwMjg3ODk0NH0." \
-          "HIO6RGgmEtBD7JaSvNEM5EX2C-EbVQH8ZmjHIExD53Q"
-
-
-# API_Key is public; No private data is exposed.
+          "HIO6RGgmEtBD7JaSvNEM5EX2C-EbVQH8ZmjHIExD53Q"  # replace with your actual API Key
 
 
 def unix_ms_to_datetime(millis):
-    """
-    Convert unix time in milliseconds to a datetime object.
-
-    Args:
-        millis (int): Unix time in milliseconds.
-
-    Returns:
-        datetime.datetime: A datetime object representing the given unix time.
-    """
-    return datetime.fromtimestamp(millis / 1000.0)  # Divide by 1000 to convert to seconds
+    return datetime.fromtimestamp(millis / 1000.0)
 
 
-def format_url_params(*params):
-    """
-       Concatenates all input parameters with "&" for URL formatting.
-
-       :type params: str
-       :return: a string concatenated with "&"
-       """
-    return '&'.join(params)
+def date_to_unix_ms(date):
+    dt = datetime(date.year, date.month, date.day)
+    return int(dt.timestamp() * 1000)
 
 
-@st.cache_data
 def make_request(path, *params):
-    """
-       Makes a GET request to the API.
-
-       The function concatenates the API url, path, apikey, and any extra parameters
-       to form the complete URL, then makes a GET request to that URL.
-
-       If the request is successful, the function returns the response in JSON format.
-
-       :param path: The API endpoint.
-       :param params: Extra parameters to add to the API URL.
-       :return: The response in JSON format.
-       """
-
-    param_string = format_url_params(*params)
-    url = f"{api_url}{path}?apikey={api_key}&{param_string}"
-
-    response = requests.get(url)
+    param_string = '&'.join(params)
+    url = f"{API_URL}{path}"
+    headers = {'apikey': API_KEY}
+    response = requests.get(url, params=param_string, headers=headers)
 
     if response.ok:
         return response.json()
@@ -65,11 +33,41 @@ def make_request(path, *params):
         return response.json()
 
 
-channels = make_request("channels")
-usernames = make_request("usernames")
-rolls = make_request("rolls", "select=unix_milliseconds, dice_value"
-                              "channel:channels(name:channel_name),"
-                              "user:usernames(name:username)")
+def get_date_of_previous_sunday(weeks_before: int = 1) -> date:
+    today = date.today()
+    date_weeks_before = today - timedelta(weeks=weeks_before)
+    days_to_subtract = (date_weeks_before.weekday() + 1) % 7
+    previous_sunday = date_weeks_before - timedelta(days=days_to_subtract)
+    return previous_sunday
 
-for roll in rolls:
-    roll["date_time"] = unix_ms_to_datetime(roll["unix_milliseconds"])
+
+sunday = get_date_of_previous_sunday(2)
+ums = date_to_unix_ms(sunday)
+
+rolls = make_request("rolls",
+                     "select=unix_milliseconds,dice_value,"
+                     "channel:channels(name:channel_name),"
+                     "username:usernames(name:username)",
+                     f"unix_milliseconds=gt.{ums}")
+
+df = pd.DataFrame(rolls)
+
+df["date_time"] = pd.to_datetime(df["unix_milliseconds"], unit='ms')
+df["channel"] = df["channel"].apply(lambda x: x['name'])
+df["username"] = df["username"].apply(lambda x: x['name'])
+
+df.set_index('date_time', inplace=True)
+
+df.sort_index(inplace=True)
+
+
+# Create a new column with mean dice_value
+df['mean'] = np.mean(df['dice_value'])
+
+st.write("[API Documentation](https://app.swaggerhub.com/apis/OLSON_1/JavaJotterAPI/10.2.0)")
+
+# Use Streamlit to plot the data
+st.line_chart(df[['dice_value', 'mean']])
+
+
+st.write(df)
